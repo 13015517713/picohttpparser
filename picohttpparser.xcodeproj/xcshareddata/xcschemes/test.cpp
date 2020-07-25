6aa3c8957046ccec4111204d14dcdff872fd6014
13015517713 <2430278602@qq.com>
2020-07-25 21:20:25 +0800

    test add

---
+++ picohttpparser.xcodeproj/xcshareddata/xcschemes/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iostream>
+using namespace std;
+enum node{
+     one=1,
+     #define one one
+     two,
+    //  #define two two
+
+};
+int main(){
+    cout << two <<endl;
+}
